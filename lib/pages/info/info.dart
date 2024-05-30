81eb847b2cde65314f81adec68ae1867b05f4f5c
MishelBravo <141872338+MishelBravo@users.noreply.github.com>
2024-05-30 02:30:36 -0400

    Page Info Realizado

--- lib/pages/info/info.dart
+++ lib/pages/info/info.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
 import 'package:url_launcher/url_launcher.dart';
+
+const Color colorPage4 = Color.fromARGB(255, 239, 234, 225);
+const Color colorCabecera4 = Color(0xFF387990);
 
 class InfoApp extends StatefulWidget {
   const InfoApp({Key? key}) : super(key: key);
@@ -12,290 +14,241 @@ class InfoApp extends StatefulWidget {
 class _InfoAppState extends State<InfoApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          backgroundColor: Colors.blue.shade900,
-          extendBodyBehindAppBar: true,
-          appBar: AppBar(
-            backgroundColor: Colors.transparent,
-            elevation: 0,
-            /*leading: InkWell(
-          onTap: () => ZoomDrawer.of(context)!.toggle(),
+    return Scaffold(
+      backgroundColor: colorPage4, // Color de fondo de la página
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: Text(
+          "Acerca de ",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: colorCabecera4, // Color de la cabecera
+        elevation: 0,
+        leading: InkWell(
+          onTap: () => Navigator.of(context).pop(),
           child: Icon(
-            Icons.menu,
+            Icons.arrow_back_rounded,
             color: Colors.white,
             size: 28,
           ),
-        ),*/
-            leading: InkWell(
-              onTap: () => Navigator.of(context).pop(),
-              child: Icon(
-                Icons.arrow_back_rounded,
-                color: Colors.white,
-                size: 28,
-              ),
-            ),
-          ),
-
-          ///instanciacion y configuraion del cuerpo de la ventana
-          body: Container(
-            child: Column(
-              children: <Widget>[
-                Expanded(
-                  ///instanciacion de elementos de la lista
-                  child: ListView.builder(
-                    itemCount: 1,
-                    itemBuilder: (context, index) {
-                      return Center(
-                        child: Padding(
-                          padding: EdgeInsets.all(0),
-
-                          ///divicion en columnas
-                          child: Column(
-                            children: <Widget>[
-                              Container(
-                                alignment: Alignment.center,
-                                child: Image.asset(
-                                  'assets/img/kaypi.png',
-                                  width: 170,
-                                ),
-                              ),
-                              /*Container(
-                                margin: EdgeInsets.all(20),
-                                alignment: Alignment.center,
-                                child: Text('Kaypi',
-                                  style: TextStyle(
-                                  color: Colors.blue[700],
-                                  letterSpacing: 2.0,
-                    
-                                    fontFamily: 'AlfaSlabOne',
-                                    fontSize: 28.0,
-                                    fontWeight: FontWeight.bold
-                                  ),
-                                ),
-                              ),*/
-                              ///divicion de partes dentro de la ventana
-                              ///
-                              ///intanciacion de una imagen (logotipo)
-                              Align(
-                                alignment: Alignment.center,
-                                child: Image.asset(
-                                  'assets/img/uni.png',
-                                  width: 150,
-                                  height: 150,
-                                ),
-                              ),
-
-                              ///asignacion de tamaño
-                              SizedBox(height: 10.0),
-
-                              ///salto de linea
-                              Align(
-                                alignment: Alignment.center,
-                                child: Text(
-                                  '\n',
-                                  textAlign: TextAlign.center,
-                                ),
-                              ),
-
-                              /// version de aplicacion y configuracion de visualizacion
-                              Align(
-                                alignment: Alignment.center,
-                                child: Text(
-                                  'Versión 2.0',
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white,
-                                    letterSpacing: 2.0,
-                                  ),
-                                ),
-                              ),
-
-                              SizedBox(
-                                height: 20,
-                              ),
-
-                              /// llamado a las card para su implementacion
-                              miCard(),
-                              miCard2(),
-
-                              Divider(
-                                height: 90.0,
-                                color: Colors.white,
-                              ),
-
-                              ///divicion de partes dentro de la ventana
-                              /// inicio de otras aplicaciones
-                              Align(
-                                alignment: Alignment.center,
-                                child: Text(
-                                  'Otras Aplicaciones',
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    letterSpacing: 2.0,
-                                  ),
-                                ),
-                              ),
-
-                              SizedBox(height: 30.0),
-
-                              /// llamado a los card para su implementacion
-                              miCard3(),
-                              miCardDesign(),
-                            ],
+        ),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: ListView.builder(
+                itemCount: 1,
+                itemBuilder: (context, index) {
+                  return Center(
+                    child: Padding(
+                      padding: EdgeInsets.all(0),
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            alignment: Alignment.center,
+                            child: Image.asset(
+                              'assets/img/KaypiNegro.png',
+                              width: 170,
+                            ),
                           ),
-                        ),
-                      );
-                    },
-                  ),
-                )
-              ],
+                          Align(
+                            alignment: Alignment.center,
+                            child: Image.asset(
+                              'assets/img/uni.png',
+                              width: 150,
+                              height: 150,
+                            ),
+                          ),
+                          SizedBox(height: 10.0),
+                          Align(
+                            alignment: Alignment.center,
+                            child: Text(
+                              '\n',
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment.center,
+                            child: Text(
+                              'Versión 2.0',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: const Color.fromARGB(255, 0, 0, 0),
+                                letterSpacing: 2.0,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          miCard(),
+                          miCard2(),
+                          Divider(
+                            height: 90.0,
+                            color: const Color.fromARGB(255, 0, 0, 0),
+                          ),
+                          Align(
+                            alignment: Alignment.center,
+                            child: Text(
+                              'Otras Aplicaciones',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: const Color.fromARGB(255, 0, 0, 0),
+                                letterSpacing: 2.0,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 30.0),
+                          miCard3(),
+                          miCardDesign(),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Card miCard() {
+  return Card(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+    elevation: 10,
+    child: Padding(
+      padding: const EdgeInsets.all(10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          SizedBox(
+            width: 60,
+            height: 60,
+            child: FadeInImage(
+              placeholder: AssetImage('assets/img/loading.gif'),
+              image: AssetImage('assets/img/descargacocha.png'),
+              fit: BoxFit.cover,
             ),
           ),
-        ));
-  }
-
-  ///instanciacion de la clase micard
-  Card miCard() {
-    return Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      margin: EdgeInsets.all(10),
-      elevation: 10,
-      child: Column(
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              Align(
-                alignment: Alignment.centerRight,
-                child: Text("       "),
+          SizedBox(width: 20),
+          Expanded(
+            child: Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.zero,
+              child: Text(
+                "Contribuciones\n\nGobierno Municipal de Cochabamba",
+                textAlign: TextAlign.left,
               ),
-              FadeInImage(
-                alignment: Alignment.centerRight,
-                placeholder: AssetImage('assets/img/loading.gif'),
-                image: AssetImage('assets/img/descargacocha.png'),
-                height: 60,
-                width: 60,
-              ),
-              Container(
-                margin: EdgeInsets.all(0),
-                alignment: Alignment.topLeft,
-                padding: EdgeInsets.fromLTRB(10, 20, 50, 20),
-                child:
-                    Text("Contribuciones\n\nGobierno Municipal de Cochabamba"),
-              ),
-            ],
-          )
+            ),
+          ),
         ],
       ),
-    );
-  }
+    ),
+  );
+}
 
-  ///instanciacion de la clase micard2
   Card miCard2() {
-    return Card(
-      ///configuracion de visualizacion
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      margin: EdgeInsets.all(10),
-      elevation: 10,
-      child: Column(
-        ///division en columnas
-        ///instanciacion del widget
+  return Card(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+    margin: EdgeInsets.all(10),
+    elevation: 10,
+    child: Padding(
+      padding: const EdgeInsets.all(10),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              Align(
-                alignment: Alignment.centerRight,
-                child: Text("       "),
+          SizedBox(
+            width: 40,
+            height: 40,
+            child: FadeInImage(
+              placeholder: AssetImage('assets/img/loading.gif'),
+              image: AssetImage('assets/img/uni.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(width: 10),
+          Expanded(
+            child: Container(
+              alignment: Alignment.topLeft,
+              padding: EdgeInsets.all(10),
+              child: Text(
+                "Desarrollado por:\nUniversidad Privada Del Valle\n\nDesarrolladores:\n\nGabriel Sebastian Clavijo\nLuis Ángel Jallasa\nMirko Marca\nMiguel Angel Terrazas\nHeidi Ivanna Huanca\nAxel Eddy Martinez\nCristopher Joaquin Jimenez\nEric Emmanuel Galleguillos\nSergio Lara Rocabado\nJimena Gonzales\nAxel Matias Miranda\nCarolina Vivian Escobar\nPaulo David Crespo\nNoemi Sanchez\nEdward Rene Jimenez\nMichel Sanabria",
+                style: TextStyle(
+                  color: Colors.grey.shade800,
+                  fontSize: 16.0,
+                ),
               ),
-
-              ///insercion de imagen
-              FadeInImage(
-                alignment: Alignment.centerRight,
-                placeholder: AssetImage('assets/img/loading.gif'),
-                image: AssetImage('assets/img/uni.png'),
-                height: 40,
-                width: 40,
-              ),
-
-              /// contenerdor donde se muestra la informacion de los desarrolladores
-              Container(
-                margin: EdgeInsets.all(0),
-                alignment: Alignment.topLeft,
-                padding: EdgeInsets.fromLTRB(20, 30, 100, 30),
-                child: Text(
-                    "Desarrollado por:\nUniversidad Privada Del Valle\n\nDesarrolladores:\n\nGabriel Sebastian Clavijo\nLuis Ángel Jallasa\nMirko Marca\nMiguel Angel Terrazas\nHeidi Ivanna Huanca\nAxel Eddy Martinez\nCristopher Joaquin Jimenez\nEric Emmanuel Galleguillos\nSergio Lara Rocabado\nJimena Gonzales\nAxel Matias Miranda\nCarolina Vivian Escobar\nPaulo David Crespo\nNoemi Sanchez\nEdward Rene Jimenez\nMichel Sanabria"),
-              ),
-            ],
-          )
+            ),
+          ),
         ],
       ),
-    );
-  }
+    ),
+  );
+}
 
-  ///instanciacion de la clase micard3
   Card miCard3() {
-    return Card(
-      ///configuracion de visualizacion
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      margin: EdgeInsets.all(10),
-      elevation: 10,
-      child: Column(
-        ///division en columnas
-        ///instanciacion del widget
+  return Card(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+    margin: EdgeInsets.all(10),
+    elevation: 10,
+    child: Padding(
+      padding: const EdgeInsets.all(10),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Row(
-            children: <Widget>[
-              Align(
-                alignment: Alignment.centerRight,
-                child: Text("       "),
+          SizedBox(
+            width: 30,
+            height: 30,
+            child: FadeInImage(
+              placeholder: AssetImage('assets/img/loading.gif'),
+              image: AssetImage('assets/img/tramitecochabamba.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(width: 10),
+          Expanded(
+            child: Container(
+              alignment: Alignment.topLeft,
+              padding: EdgeInsets.fromLTRB(5, 10, 40, 0),
+              child: Text(
+                "Trámites Cochabamba\n\n",
+                style: TextStyle(
+                  color: Colors.grey.shade800,
+                  fontSize: 16.0,
+                ),
               ),
-
-              ///insercion de imagen
-              FadeInImage(
-                alignment: Alignment.centerRight,
-                placeholder: AssetImage('assets/img/loading.gif'),
-                image: AssetImage('assets/img/tramitecochabamba.png'),
-                height: 30,
-                width: 30,
+            ),
+          ),
+          Center(
+            child: MaterialButton(
+              color: Colors.blueAccent,
+              textColor: Colors.white,
+              child: Icon(
+                Icons.download,
+                size: 24,
               ),
-
-              /// contenerdor donde se muestra tramites cbba
-              Container(
-                margin: EdgeInsets.all(20),
-                alignment: Alignment.topLeft,
-                padding: EdgeInsets.fromLTRB(5, 10, 40, 0),
-                child: Text("Trámites Cochabamba\n\n"),
-              ),
-
-              /// enlace para redirigir a la descarga de la aplicacion el googleplaystore
-              Center(
-                child: MaterialButton(
-                    color: Colors.blueAccent,
-                    textColor: Colors.white,
-                    child: Icon(
-                      Icons.download,
-                      size: 24,
-                    ),
-                    padding: EdgeInsets.all(16),
-                    shape: CircleBorder(),
-                    onPressed: () {
-                      launch(
-                          "https://play.google.com/store/apps/details?id=bo.tramitesco.chabamba&hl=es&gl=US");
-                    }),
-              ),
-            ],
-          )
+              padding: EdgeInsets.all(16),
+              shape: CircleBorder(),
+              onPressed: () {
+                launch(
+                  "https://play.google.com/store/apps/details?id=bo.tramitesco.chabamba&hl=es&gl=US",
+                );
+              },
+            ),
+          ),
         ],
       ),
-    );
-  }
+    ),
+  );
+}
+
+  
 
   ///instanciacion de la clase micardDesign
   Card miCardDesign() {
